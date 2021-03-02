e3b50eb2c531c984edfe4360a6a0fccf989347ae
Mr-DareDevil <arnabsen1729@gmail.com>
2021-03-02 14:04:57 +0530

    Typo Fixed: class to className

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 
 const Hero = () => {
     return (
-        <section class="text-gray-600 body-font">
-            <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <section className="text-gray-600 body-font">
+            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
                 <img
-                    class="lg:w-2/6 md:w-3/6 w-5/6 mb-5 object-cover object-center rounded"
+                    className="lg:w-2/6 md:w-3/6 w-5/6 mb-5 object-cover object-center rounded"
                     alt="hero"
                     src={process.env.PUBLIC_URL + 'logo.png'}
                 />
-                <div class="text-center lg:w-2/3 w-full">
-                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                <div className="text-center lg:w-2/3 w-full">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                         React and Tailwind CSS Starter Kit
                     </h1>
-                    <p class="mb-8 leading-relaxed">
+                    <p className="mb-8 leading-relaxed">
                         Meggings kinfolk echo park stumptown DIY, kale chips
                         beard jianbing tousled. Chambray dreamcatcher trust
                         fund, kitsch vice godard disrupt ramps hexagon mustache
@@ -21,11 +21,11 @@ const Hero = () => {
                         meditation PBR&amp;B pickled ennui celiac mlkshk freegan
                         photo booth af fingerstache pitchfork.
                     </p>
-                    <div class="flex justify-center">
-                        <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                    <div className="flex justify-center">
+                        <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
                             Button
                         </button>
-                        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
                             Button
                         </button>
                     </div>
